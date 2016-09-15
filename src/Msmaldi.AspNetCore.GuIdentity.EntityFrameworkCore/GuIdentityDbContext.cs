a567f51b932fe47e01b47fae4875e56004b74bd2
Matheus Soares Maldi <msmaldi@hotmail.com>
2016-09-15 09:07:59 -0300

    Update Version 1.1.0

--- src/Msmaldi.AspNetCore.GuIdentity.EntityFrameworkCore/GuIdentityDbContext.cs
+++ src/Msmaldi.AspNetCore.GuIdentity.EntityFrameworkCore/GuIdentityDbContext.cs
@@ -7,21 +7,30 @@ namespace Msmaldi.AspNetCore.GuIdentity.EntityFrameworkCore
 	public class GuIdentityDbContext : GuIdentityDbContext<GuIdentityUser>
 	{		
 		public GuIdentityDbContext(DbContextOptions options) : base(options)
-		{			
-		}
+		{ }
 
 		protected GuIdentityDbContext()
-		{			
-		}
+		{ }
 	}
-	public class GuIdentityDbContext<TUser> : IdentityDbContext<TUser, GuIdentityRole, Guid> where TUser : GuIdentityUser
+	
+	public class GuIdentityDbContext<TUser> : GuIdentityDbContext<TUser, GuIdentityRole>
+		where TUser : GuIdentityUser
+	{		
+		public GuIdentityDbContext(DbContextOptions options) : base(options)
+		{ }
+
+		protected GuIdentityDbContext()
+		{ }
+	}
+
+	public class GuIdentityDbContext<TUser, TRole> : IdentityDbContext<TUser, TRole, Guid>
+		where TUser : GuIdentityUser
+		where TRole : GuIdentityRole
 	{
 		public GuIdentityDbContext(DbContextOptions options) : base(options)
-		{			
-		}
+		{ }
 
 		protected GuIdentityDbContext()
-		{			
-		}
+		{ }
 	}
 }
--- src/Msmaldi.AspNetCore.GuIdentity.EntityFrameworkCore/GuIdentityRole.cs
+++ src/Msmaldi.AspNetCore.GuIdentity.EntityFrameworkCore/GuIdentityRole.cs
@@ -4,6 +4,13 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 namespace Msmaldi.AspNetCore.GuIdentity.EntityFrameworkCore
 {
 	public class GuIdentityRole : IdentityRole<Guid>
-	{		
+	{
+		public GuIdentityRole()
+		{ }
+
+		public GuIdentityRole(string roleName) : this()
+        {
+            Name = roleName;
+        }
 	}
 }
